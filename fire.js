da1065be7e9a582b593a2d1788befae30986270d
Jiani Liu <36114969+687000@users.noreply.github.com>
2019-11-21 16:08:57 -0800

    Update fire.js

--- fire.js
+++ fire.js
@@ -15,18 +15,15 @@ $(document).ready(function() {
     var canvasWidth = 320;
     var canvasHeight = 480;
     $("#surface").css({
-        width: canvasWidth/2,
-        height: canvasHeight/2
+        width: 300,
+        height: 500
     });
 
     // shim layer with setTimeout fallback from Paul Irish
     window.requestAnimFrame = (function() {
         return window.requestAnimationFrame ||
-            window.webkitRequestAnimationFrame ||
-            window.mozRequestAnimationFrame ||
             function(callback) {
-
-                window.setTimeout(callback, 6000 / 60);
+                window.setTimeout(callback,1);
             };
     })();
 
@@ -35,32 +32,27 @@ $(document).ready(function() {
             x: -1 + Math.random() * 2,
             y: -5 + Math.random() * 5
         };
-        canvasWidth = (document.getElementById("surface").width);
-        canvasHeight = (document.getElementById("surface").height);
         this.location = {
-            x: canvasWidth / 2,
-            y: (canvasHeight / 2) + 35
+          x: 150,
+          y: 80,
         };
-        this.radius = .5 + Math.random() * 3;
-        this.life = 10 + Math.random() * 10;
+        this.radius = .5 + Math.random() * 1;
+        this.life = 1 + Math.random() * 8;
         this.death = this.life;
         this.r = 255;
-        this.g = Math.round(Math.random() * 155);
+        this.g = Math.random() * 155;
         this.b = 0;
     }
 
     function ParticleAnimation() {
         surface.globalCompositeOperation = "source-in";
         surface.fillStyle = "rgba(0, 0, 0, 0)";
-        surface.fillRect(0, 0, canvasWidth, canvasHeight);
+        surface.fillRect(0,0,50,100);
         surface.globalCompositeOperation = "lighter";
-
         for (var i = 0; i < particles.length; i++) {
             var p = particles[i];
-
             surface.beginPath();
-
-            p.opacity = Math.round(p.death / p.life * 100) / 100
+            p.opacity =Math.random()/2;
             var gradient = surface.createRadialGradient(p.location.x, p.location.y, 0, p.location.x, p.location.y, p.radius);
             gradient.addColorStop(0, "rgba(" + p.r + ", " + p.g + ", " + p.b + ", " + p.opacity + ")");
             gradient.addColorStop(0.5, "rgba(" + p.r + ", " + p.g + ", " + p.b + ", " + p.opacity + ")");
@@ -70,9 +62,8 @@ $(document).ready(function() {
             surface.fill();
             p.death--;
             p.radius++;
-            p.location.x += (p.speed.x);
+            p.location.x += (p.speed.x/1.2);
             p.location.y += (p.speed.y);
-
             //regenerate particles
             if (p.death < 0 || p.radius < 0) {
                 //a brand new particle replacing the dead one
@@ -80,9 +71,6 @@ $(document).ready(function() {
             }
         }
         requestAnimFrame(ParticleAnimation);
-
     }
-
     ParticleAnimation();
-
 });
