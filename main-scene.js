6a6c772f0f612891fa774e995e5d190b96938ecf
Jiani Liu <36114969+687000@users.noreply.github.com>
2019-11-26 15:12:23 -0800

    Update main-scene.js
    add mouse picking

--- main-scene.js
+++ main-scene.js
@@ -59,10 +59,10 @@ class Cube_Outline extends Shape
       { super( "positions", "colors" ); // Name the values we'll define per each vertex.
       this.positions.push( ...Vec.cast( [-1,-1,-1], [1,-1,-1], [-1,-1,1], [1,-1,1], [1,1,-1],  [-1,1,-1],  [1,1,1],  [-1,1,1],
                                         [-1,-1,-1], [-1,-1,1], [-1,1,-1], [-1,1,1], [1,-1,1],  [1,-1,-1],  [1,1,1],  [1,1,-1],
-                                        [1,1,-1],  [1,-1,-1],  [1,1,1], [1,-1,1], [-1,1,1], [-1,-1,1], [-1,1,-1], [-1,-1,-1] ) );                                   
-      this.colors.push( ...Vec.cast( [1,1,1,1], [1,1,1,1], [1,1,1,1], [1,1,1,1], [1,1,1,1], [1,1,1,1], [1,1,1,1], [1,1,1,1], 
-                                     [1,1,1,1], [1,1,1,1], [1,1,1,1], [1,1,1,1], [1,1,1,1], [1,1,1,1], [1,1,1,1], [1,1,1,1], 
-                                     [1,1,1,1], [1,1,1,1], [1,1,1,1], [1,1,1,1], [1,1,1,1], [1,1,1,1], [1,1,1,1], [1,1,1,1] ) );  
+                                        [1,1,-1],  [1,-1,-1],  [1,1,1], [1,-1,1], [-1,1,1], [-1,-1,1], [-1,1,-1], [-1,-1,-1] ) );
+      this.colors.push( ...Vec.cast( [1,1,1,1], [1,1,1,1], [1,1,1,1], [1,1,1,1], [1,1,1,1], [1,1,1,1], [1,1,1,1], [1,1,1,1],
+                                     [1,1,1,1], [1,1,1,1], [1,1,1,1], [1,1,1,1], [1,1,1,1], [1,1,1,1], [1,1,1,1], [1,1,1,1],
+                                     [1,1,1,1], [1,1,1,1], [1,1,1,1], [1,1,1,1], [1,1,1,1], [1,1,1,1], [1,1,1,1], [1,1,1,1] ) );
       this.indexed = false;       // Do this so we won't need to define "this.indices".
         //  TODO (Requirement 5).
                                 // When a set of lines is used in graphics, you should think of the list entries as
@@ -77,11 +77,11 @@ class Cube_Single_Strip extends Shape
   { constructor()
       { super( "positions", "normals" );
         this.positions.push( ...Vec.cast( [-1,1,1],[1,1,1],[-1,-1,1],[1,-1,1],[1,-1,-1],[1,1,1],[1,1,-1],
-                                          [-1,1,1],[-1,1,-1],[-1,-1,1],[-1,-1,-1],[1,-1,-1],[-1,1,-1],[1,1,-1] ) );                            
+                                          [-1,1,1],[-1,1,-1],[-1,-1,1],[-1,-1,-1],[1,-1,-1],[-1,1,-1],[1,1,-1] ) );
 
         this.normals.push(   ...Vec.cast(  [-1,1,1],[1,1,1],[-1,-1,1],[1,-1,1],[1,-1,-1],[1,1,1],[1,1,-1],
-                                           [-1,1,1],[-1,1,-1],[-1,-1,1],[-1,-1,-1],[1,-1,-1],[-1,1,-1],[1,1,-1] ) );       
-                                       
+                                           [-1,1,1],[-1,1,-1],[-1,-1,1],[-1,-1,-1],[1,-1,-1],[-1,1,-1],[1,1,-1] ) );
+
         this.indexed = false;
       }
   }
@@ -90,8 +90,72 @@ window.Assignment_One_Scene = window.classes.Assignment_One_Scene =
 class Assignment_One_Scene extends Scene_Component
   { constructor( context, control_box )     // The scene begins by requesting the camera, shapes, and materials it will need.
       { super(   context, control_box );    // First, include a secondary Scene that provides movement controls:
+        [ this.context,this.gl,this.framebuffer,this.pixels,this.mousepicking] = [ context,null,null,Vec.of(1,0,0,0),null ];                  // Data members
         if( !context.globals.has_controls   )
           context.register_scene_component( new Movement_Controls( context, control_box.parentElement.insertCell() ) );
+          var canvas = document.getElementsByTagName("canvas")[1];
+          this.gl = context.canvas.getContext("webgl")||context.canvas.getContext("experimental-webgl");
+          var width = canvas.width;
+          var height = canvas.height;
+            //this.gl.enable(this.gl.CULL_FACE);
+            var texture = this.gl.createTexture();
+            this.gl.bindTexture(this.gl.TEXTURE_2D, texture);
+            //this.gl.pixelStorei(this.gl.UNPACK_FLIP_Y_WEBGL, true);
+            this.gl.texImage2D(this.gl.TEXTURE_2D, 0,this.gl.RGB, width, height, 0,this.gl.RGB,this.gl.UNSIGNED_BYTE, null);
+            this.gl.generateMipmap(this.gl.TEXTURE_2D);
+            var renderbuffer = this.gl.createRenderbuffer();
+            this.gl.bindRenderbuffer(this.gl.RENDERBUFFER, renderbuffer);
+            this.gl.renderbufferStorage(this.gl.RENDERBUFFER, this.gl.DEPTH_COMPONENT16, width, height);
+                  // Get WebGLRenderingContext from canvas element.
+
+            this.framebuffer = this.gl.createFramebuffer();
+            this.gl.bindFramebuffer(this.gl.FRAMEBUFFER, this.framebuffer);
+            this.gl.framebufferTexture2D(this.gl.FRAMEBUFFER, this.gl.COLOR_ATTACHMENT0, this.gl.TEXTURE_2D, texture, 0);
+            this.gl.framebufferRenderbuffer(this.gl.FRAMEBUFFER, this.gl.DEPTH_ATTACHMENT, this.gl.RENDERBUFFER, renderbuffer);
+
+
+           var status=this.gl.checkFramebufferStatus(this.gl.FRAMEBUFFER);
+           if (status !== this.gl.FRAMEBUFFER_COMPLETE) {
+              alert(status);
+           }
+           document.addEventListener( "mousedown",   e => {
+
+            var program=this.gl.createProgram();
+    	      // off-screen rendering
+    	      this.gl.bindFramebuffer(this.gl.FRAMEBUFFER, this.framebuffer);
+    	      this.gl.uniform1i(program.uOffscreen, true);
+            this.gl.clear(this.gl.COLOR_BUFFER_BIT |this.gl.DEPTH_BUFFER_BIT);
+    	      this.display(context.globals.graphics_state);
+
+
+            var colorPicked = new Uint8Array(4);
+            this.gl.readPixels(width/2,height/2, 1, 1, this.gl.RGBA, this.gl.UNSIGNED_BYTE, colorPicked);
+            this.pixels=colorPicked;
+            if(colorPicked[0]==255&&colorPicked[1]==255&&colorPicked[2]==255){
+              this.mousepicking="door";
+            }
+            else if(colorPicked[0]==0&&colorPicked[1]==0&&colorPicked[2]==0){
+              this.mousepicking="key";
+            }
+            else{
+              this.mousepicking=null;
+            }e
+            // on-screen rendering
+    	      this.gl.bindFramebuffer(this.gl.FRAMEBUFFER, null);
+    	      this.gl.uniform1i(program.uOffscreen, false);
+            this.display(context.globals.graphics_state);
+           } );
+
+
+
+      //     this.gl.bindFramebuffer(this.gl.FRAMEBUFFER, this.framebuffer);
+
+
+
+            this.gl.bindTexture(this.gl.TEXTURE_2D, null);
+            this.gl.bindRenderbuffer(this.gl.RENDERBUFFER, null);
+            this.gl.bindFramebuffer(this.gl.FRAMEBUFFER, null);
+
 
         const r = context.width/context.height;
         context.globals.graphics_state.    camera_transform = Mat4.translation([ 0,5,0 ]);  // Locate the camera here (inverted matrix).
@@ -108,10 +172,10 @@ class Assignment_One_Scene extends Scene_Component
                                                           // same thing here.
 
                                      // Make some Material objects available to you:
-        this.clay   = context.get_instance( Phong_Shader ).material( Color.of( .9,.5,.9, 1 ), { ambient: 0.3, diffusivity: 0.8,specular:0.4 } );
+        this.clay   = context.get_instance( Phong_Shader ).material( Color.of( .9,.5,.9, 1 ), { ambient: 0.2, diffusivity: 0.8,specular:0.4 } );
         this.white  = context.get_instance( Basic_Shader ).material();
         this.plastic = this.clay.override({ specularity: .6 });
-
+        this.target   = context.get_instance( Phong_Shader ).material( Color.of( .9,.5,.9, 1 ), { ambient: 1, diffusivity: 0.8,specular:0.4 } );
         this.lights = [ new Light( Vec.of( 0,5,5,1 ), Color.of( 1, 0, 0, 1 ), 10000 ) ];
 
         this.materials =
@@ -122,18 +186,17 @@ class Assignment_One_Scene extends Scene_Component
             p4_material:     context.get_instance( Phong_Shader ).material( Color.of( 0.1,0.1,0.9,1 ), { specular: 0.8 } ),
             p5_material:     context.get_instance( Phong_Shader ).material( Color.of( 0.74,0.7,0.76,1 ), { diffusivity: 1 ,specular: 1 } )
 
-
                                 // TODO:  Fill in as many additional material objects as needed in this key/value table.
                                 //        (Requirement 1)
           }
-        
+
         this.rgba=[];
         this.outline_flag=false;
         this.rotate_flag=true;
         this.set_outline_flag();
         this.set_colors();
         this.set_rotate();
-        
+
       }
     set_outline_flag(){
           this.outline_flag=!this.outline_flag;
@@ -148,13 +211,31 @@ class Assignment_One_Scene extends Scene_Component
     set_rotate(){
           this.rotate_flag=!this.rotate_flag;
     }
-    
+
     make_control_panel()             // Draw the scene's buttons, setup their actions and keyboard shortcuts, and monitor live measurements.
-      { 
+      {
         this.key_triggered_button( "Change Colors", [ "c" ], this.set_colors );    // Add a button for controlling the scene.
         this.key_triggered_button( "Outline",       [ "o" ], this.set_outline_flag );
         this.key_triggered_button( "Sit still",     [ "m" ], this.set_rotate );
+        this.live_string( box => box.textContent = this.mousepicking);
+        this.new_line();
+        this.live_string( box => box.textContent = this.pixels);
+        this.new_line();
       }
+    render(graphics_state, model_transform) {
+      const yellow = Color.of( 1,1,0,1 );
+      var program=this.gl.createProgram();
+    	// off-screen rendering
+    	this.gl.bindFramebuffer(this.gl.FRAMEBUFFER, this.framebuffer);
+    	this.gl.uniform1i(program.uOffscreen, true);
+    	//this.shapes.box.draw( graphics_state, model_transform,this.plastic.override({ color: yellow }));
+
+    	// on-screen rendering
+    	this.gl.bindFramebuffer(this.gl.FRAMEBUFFER, null);
+    	this.gl.uniform1i(program.uOffscreen, false);
+      //this.shapes.box.draw( graphics_state, model_transform,this.plastic.override({ color: yellow }));
+
+    }
     draw_box( graphics_state, model_transform,index )
       {
         // TODO:  Helper function for requirement 3 (see hint).
@@ -166,7 +247,7 @@ class Assignment_One_Scene extends Scene_Component
         else {
           if( !this.outline_flag ) {
             this.shapes.outline.draw( graphics_state, model_transform, this.white, "LINES"  );
-          }  
+          }
           else {
             this.shapes.box.draw( graphics_state, model_transform, this.plastic.override({color: this.rgba[index] } ) );
           }
@@ -176,17 +257,17 @@ class Assignment_One_Scene extends Scene_Component
           model_transform = model_transform.times( Mat4.translation(Vec.of(1,1,1)));
           model_transform = model_transform.times( Mat4.rotation( -0.02*Math.PI-0.02*Math.PI*Math.sin(20*this.t), Vec.of( 0,0,1 ) ) );
           model_transform = model_transform.times( Mat4.translation(Vec.of(-1,1,-1)));
-        } 
+        }
         else{
           model_transform = model_transform.times( Mat4.translation(Vec.of(1,1,1)));
           model_transform = model_transform.times( Mat4.rotation( -0.02*Math.PI-0.02*Math.PI*1, Vec.of( 0,0,1) ) );
           model_transform = model_transform.times( Mat4.translation(Vec.of(-1,1,-1)));
-          
+
         }
         return model_transform;
       }
     display( graphics_state )
-      { 
+      {
         const t = graphics_state.animation_time / 1000, dt = graphics_state.animation_delta_time / 1000;
         var fire_pos=(Mat4.inverse(graphics_state.camera_transform)).times(Vec.of(0,0,0,1));
         let light=new Light( fire_pos, Color.of(1,0.2,0,1), 5+Math.random()*3);
@@ -195,29 +276,29 @@ class Assignment_One_Scene extends Scene_Component
         //graphics_state.lights = this.lights;
         graphics_state.lights = this.lights;        // Use the lights stored in this.lights.
         const blue = Color.of( 0,0,1,1 ), yellow = Color.of( 1,1,0,1 );
-        
+
         let model_transform = Mat4.identity();
         model_transform=Mat4.scale([10,8,0.1]).times(model_transform);
         model_transform=Mat4.translation([0,0,-10]).times(model_transform);
         this.shapes.box.draw( graphics_state, model_transform, this.plastic.override({ color: blue }));
-        
-        
-
+        //this.render(graphics_state, model_transform);
         model_transform = Mat4.identity();
         model_transform=Mat4.scale([0.1,8,10]).times(model_transform);
         model_transform=Mat4.translation([-10,0,0]).times(model_transform);
         this.shapes.box.draw( graphics_state, model_transform,this.plastic.override({ color: yellow }));
-        
+
+        //this.render(graphics_state, model_transform);
+
         model_transform = Mat4.identity();
         model_transform=Mat4.scale([0.1,8,10]).times(model_transform);
         model_transform=Mat4.translation([10,0,0]).times(model_transform);
         this.shapes.box.draw( graphics_state, model_transform,this.plastic.override({ color: yellow }));
-        
+        //this.render(graphics_state, model_transform);
         model_transform = Mat4.identity();
         model_transform=Mat4.scale([10,8,0.1]).times(model_transform);
         model_transform=Mat4.translation([0,0,10]).times(model_transform);
         this.shapes.box.draw( graphics_state, model_transform,this.plastic.override({ color: blue }));
-        
+
         model_transform = Mat4.identity();
         model_transform=Mat4.scale([10,0.1,10]).times(model_transform);
         model_transform=Mat4.translation([0,-8,0]).times(model_transform);
@@ -228,6 +309,18 @@ class Assignment_One_Scene extends Scene_Component
         model_transform=Mat4.translation([0,-4,-9.9]).times(model_transform);
         this.shapes.box.draw( graphics_state, model_transform,this.plastic.override({ color: Color.of(1,1,1,1) }));
 
+//door target
+        model_transform = Mat4.identity();
+        model_transform=Mat4.scale([0.3,0.3,0.1]).times(model_transform);
+        model_transform=Mat4.translation([2,-4,-9.8]).times(model_transform);
+        this.shapes.box.draw( graphics_state, model_transform,this.target.override({ color: Color.of(1,1,1,1) }));
+//key target
+        model_transform = Mat4.identity();
+        model_transform=Mat4.scale([0.3,0.3,0.1]).times(model_transform);
+        model_transform=Mat4.translation([7,-7,7]).times(model_transform);
+        this.shapes.box.draw( graphics_state, model_transform,this.target.override({ color: Color.of(0,0,0,1) }));
+
+
         model_transform = Mat4.scale([0.2,0.5,0.2]);
         //model_transform = Mat4.translation([2,-3,2]).times(model_transform);
         //model_transform=Mat4.inverse(graphics_state.camera_transform).times(model_transform);
@@ -235,7 +328,7 @@ class Assignment_One_Scene extends Scene_Component
         //this.shapes.box.draw( graphics_state, model_transform,this.plastic.override({ color: Color.of(1,0,1,1) }));
         model_transform = Mat4.identity();
         this.shapes.planet_1.draw( graphics_state, model_transform,this.plastic.override({ color: Color.of(1,1,1,1) }));
-        
+
         // TODO:  Draw your entire scene here.  Use this.draw_box( graphics_state, model_transform ) to call your helper.
       }
   }
