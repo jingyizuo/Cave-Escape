f5895f59332148d929198eccb4c13f810e64e23c
jingyizuo <55892599+jingyizuo@users.noreply.github.com>
2019-12-02 21:34:40 -0800

    Update common.js

--- examples/common.js
+++ examples/common.js
@@ -1,9 +1,9 @@
-import {tiny} from '../tiny-graphics.js';
+import {tiny} from 'tiny-graphics.js';
                                                   // Pull these names into this module's scope for convenience:
 const { Vector, Vector3, vec, vec3, vec4, color, Matrix, Mat4, 
          Light, Shape, Material, Shader, Texture, Scene } = tiny;
 
-import {widgets} from '../tiny-graphics-widgets.js';
+import {widgets} from 'tiny-graphics-widgets.js';
 Object.assign( tiny, widgets );
 
 const defs = {};
